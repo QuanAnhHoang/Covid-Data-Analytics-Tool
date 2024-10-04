bd8efd044b530ccceddc34aa05b5c470e2ea8344
QuanAnhHoang <57248889+QuanAnhHoang@users.noreply.github.com>
2024-10-04 22:10:41 +0700

    Create TabularDisplay.java

---
+++ src/main/java/com/covid/TabularDisplay.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class TabularDisplay implements Display {
+    @Override
+    public void show(List<Summary.SummaryResult> results) {
+        System.out.println("Range                 | Value");
+        System.out.println("----------------------|-------");
+        
+        for (Summary.SummaryResult result : results) {
+            String range = String.format("%-20s", result.getDateRange().toString());
+            System.out.printf("%s | %d%n", range, result.getValue());
+        }
+    }
+}
